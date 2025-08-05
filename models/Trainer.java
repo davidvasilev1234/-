e8dcb17de6e5973f9011a4924e930ddb59f55b22
davidvasilev1234 <davidvasilwv@gmail.com>
2025-08-05 22:20:17 +0300

    Update Trainer.java

--- models/Trainer.java
+++ models/Trainer.java
@@ -5,21 +5,17 @@ public class Trainer {
     private String name;
     private String specialty;
 
-    // Приватен конструктор – достъпен само за Builder-а
     private Trainer(Builder builder) {
         this.id = builder.id;
         this.name = builder.name;
         this.specialty = builder.specialty;
     }
 
-    // Getters
     public int getId() { return id; }
     public String getName() { return name; }
     public String getSpecialty() { return specialty; }
 
-    // Не е нужен getSpecialization(), освен ако не планираш нещо конкретно с него
 
-    // --- Builder вътрешен клас ---
     public static class Builder {
         private int id;
         private String name;
@@ -44,4 +40,5 @@ public class Trainer {
             return new Trainer(this);
         }
     }
+
 }
