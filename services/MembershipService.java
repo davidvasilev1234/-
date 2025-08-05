d65a681c3a60e6d343e1ff869bfb1ffc31d4c02f
davidvasilev1234 <davidvasilwv@gmail.com>
2025-08-05 22:17:54 +0300

    Update MembershipService.java

--- services/MembershipService.java
+++ services/MembershipService.java
@@ -7,16 +7,12 @@ import java.util.Map;
 
 public class MembershipService {
 
-    // ✅ Сингълтън инстанция
+
     private static MembershipService instance;
 
-    // Хранилище за членства (по userId)
     private Map<Integer, Membership> memberships = new HashMap<>();
 
-    // Приватен конструктор
     private MembershipService() {}
-
-    // ✅ Метод за достъп до инстанцията
     public static MembershipService getInstance() {
         if (instance == null) {
             instance = new MembershipService();
@@ -24,13 +20,12 @@ public class MembershipService {
         return instance;
     }
 
-    // Добавяне или актуализиране на членство
     public void addOrUpdateMembership(Membership membership) {
         memberships.put(membership.getUserId(), membership);
     }
 
-    // Връщане на членство по userId
     public Membership getMembershipByUserId(int userId) {
         return memberships.get(userId);
     }
+
 }
